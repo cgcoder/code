4c43bfd66a49ac80fb00b86a6cb79a490387a0e7
cgcoder <gopinath.chand@gmail.com>
2018-07-27 13:38:58 -0700

    Update ssh-key-auth.sh

--- ssh-key-auth.sh
+++ ssh-key-auth.sh
@@ -2,3 +2,9 @@
 ssh-keygen -t rsa -b 4096
 # this create a public and private key file. add pub to ~/.ssh/authorized_keys file in remote machines or run below command to do the same
 ssh-copy-id -i ~/.ssh/id_rsa.pub ubuntu@vlinux1
+
+###
+## this line 
+account sufficient pam_localuser.so
+## to /etc/pom.d/sshd to enable local user login in kerberos environment
+
